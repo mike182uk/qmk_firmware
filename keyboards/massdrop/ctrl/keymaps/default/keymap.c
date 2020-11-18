cfabfca227dc546c749fb4f2e08b988c925aa9ac
Michael Barrett <mike182uk@gmail.com>
2020-11-18 11:12:42 +0000

    fix bad rebase

--- keyboards/massdrop/ctrl/keymaps/default/keymap.c
+++ keyboards/massdrop/ctrl/keymaps/default/keymap.c
@@ -10,8 +10,6 @@ enum ctrl_keycodes {
     MD_BOOT,               //Restart into bootloader after hold timeout
 };
 
-<<<<<<< HEAD
-=======
 enum ctrl_rgb_mode {
     RGB_MODE_ALL,
     RGB_MODE_KEYLIGHT_AND_MODIFIER,
@@ -30,7 +28,6 @@ ctrl_config_t ctrl_config;
 
 keymap_config_t keymap_config;
 
->>>>>>> 98df4a7b99 (persist rgb changes in nvm)
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [0] = LAYOUT(
         KC_ESC,  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,             KC_PSCR, KC_SLCK, KC_PAUS, \
@@ -42,7 +39,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     ),
     [1] = LAYOUT(
         KC_POWER, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,           KC_MUTE, _______, _______, \
-        _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, EEP_RST,   KC_MPLY, KC_MSTP, KC_VOLU, \
+        _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,   KC_MPLY, KC_MSTP, KC_VOLU, \
         _______, RGB_SPD, RGB_VAI, RGB_SPI, RGB_HUI, RGB_SAI, _______, U_T_AUTO,U_T_AGCR,_______, _______, _______, _______, _______,   KC_MPRV, KC_MNXT, KC_VOLD, \
         _______, RGB_RMOD,RGB_VAD, RGB_MOD, RGB_HUD, RGB_SAD, _______, _______, _______, _______, _______, _______, _______, \
         _______, RGB_TOG, _______, _______, _______, MD_BOOT, NK_TOGG, _______, _______, _______, _______, _______,                              _______, \
@@ -142,30 +139,6 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
             return false;
         case RGB_TOG:
             if (record->event.pressed) {
-<<<<<<< HEAD
-              switch (rgb_matrix_get_flags()) {
-                case LED_FLAG_ALL: {
-                    rgb_matrix_set_flags(LED_FLAG_KEYLIGHT | LED_FLAG_MODIFIER | LED_FLAG_INDICATOR);
-                    rgb_matrix_set_color_all(0, 0, 0);
-                  }
-                  break;
-                case (LED_FLAG_KEYLIGHT | LED_FLAG_MODIFIER | LED_FLAG_INDICATOR): {
-                    rgb_matrix_set_flags(LED_FLAG_UNDERGLOW);
-                    rgb_matrix_set_color_all(0, 0, 0);
-                  }
-                  break;
-                case LED_FLAG_UNDERGLOW: {
-                    rgb_matrix_set_flags(LED_FLAG_NONE);
-                    rgb_matrix_disable_noeeprom();
-                  }
-                  break;
-                default: {
-                    rgb_matrix_set_flags(LED_FLAG_ALL);
-                    rgb_matrix_enable_noeeprom();
-                  }
-                  break;
-              }
-=======
                 switch (rgb_matrix_get_flags()) {
                     case LED_FLAG_ALL: {
                         ctrl_config.rgb_mode = RGB_MODE_KEYLIGHT_AND_MODIFIER;
@@ -193,7 +166,6 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
                     }
                 }
                 eeconfig_update_user(ctrl_config.raw);
->>>>>>> persist rgb changes in nvm
             }
             return false;
         case KC_POWER:
